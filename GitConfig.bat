2d68ce22795f11d68e658a5ea057126e7b2672ca
Lucas Rigo <183015@upf.br>
2022-03-04 21:17:51 -0300

    Commit de 04/03/2022-21h17m51s

---
+++ GitConfig.bat
@@ -0,0 +1,4 @@
+git config --global user.name "Lucas Rigo"
+git config --global user.email "183015@upf.br"
+git init --initial-branch=main
+git remote add origin https://github.com/LucasRigo/TopicosPF.git
