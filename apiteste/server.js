7bd721585f753667a15edfb348a77db0e98ee54b
Lucas Rigo <183015@upf.br>
2022-03-11 21:43:51 -0300

    Commit de 11/03/2022-21h43m51s

--- apiteste/server.js
+++ apiteste/server.js
@@ -28,3 +28,7 @@ app.get('/', (req, res) => {
    // rotas para animais
 const animais = require('./animais');
 animais(app);
+
+
+const colaboradores = require('./colaboradores');
+colaboradores(app);
