412657d2ae6463fe39e35a1f96dd2c77325799d2
Lucas Rigo <183015@upf.br>
2022-03-25 21:59:55 -0300

    Commit de 25/03/2022-21h59m55s

--- apirequisicoes/server/routes/ColaboradorRout.js
+++ apirequisicoes/server/routes/ColaboradorRout.js
@@ -2,4 +2,12 @@ const express = require('express');
 const routes = express.Router();
 const controle = require('../controller/ColaboradorCont');
 routes.route('/colaboradores').get(controle.listar);
+
+routes.route('/colaboradores').post(controle.incluir);
+
+
+routes.route('/usuarios').put(controle.alterar);
+routes.route('/usuarios/:id').delete(controle.excluir);
+routes.route('/usuarios/:id').get(controle.obterPeloId);
+routes.route('/usuarios/filtro/:filtro').get(controle.filtrar);
 module.exports = routes;
